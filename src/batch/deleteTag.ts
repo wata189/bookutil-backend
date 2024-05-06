5d12548038366d8fb1af1f80dd2e7e1765b4e3f9
sato wataru <watarusato105@gmail.com>
2024-05-06 17:06:47 +0900

    #47 タグ一括削除バッチ

---
+++ src/batch/deleteTag.ts
@@ -0,0 +1,43 @@
+////////////////deleteTag.st
+// 引数で指定したタグを各本から削除する
+
+
+import { program } from "commander";
+// コマンドライン引数をcommanderでパースする
+program.parse(process.argv);
+const deleteTag = program.args[0]
+if(!deleteTag){
+  console.error("引数でタグを指定してください");
+  process.exit(1);
+}
+
+console.log("start deleteTag")
+console.log(`delete ${deleteTag}`);
+
+import * as firestoreUtil from "../modules/firestoreUtil";
+
+
+const at = (new Date());
+firestoreUtil.tran([ async (fs:firestoreUtil.FirestoreTransaction) => {
+  const bookDocuments = await fs.getCollection(firestoreUtil.COLLECTION_PATH.T_TOREAD_BOOK, "isbn", "tags", "array-contains", deleteTag);
+  const updateBooks = bookDocuments.map((document) => {
+    console.log(document.book_name);
+    const tags:string[] = document.tags;
+    return {
+      documentId: document.documentId,
+      document: {
+        tags: tags.filter(tag => tag !== deleteTag),
+        update_user: "batch",
+        update_at: at,
+      }
+    }
+  })
+
+  const promises:Promise<void>[] = [];
+  updateBooks.forEach((book) => {
+    promises.push(fs.updateDocument(firestoreUtil.COLLECTION_PATH.T_TOREAD_BOOK, book.documentId, book.document)) ;
+  })
+  const results = (await Promise.all(promises));
+
+  console.log("end insertMLibrary")
+}]);
